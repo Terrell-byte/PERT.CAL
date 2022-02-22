02fab226e5cdb7ad40eb59b99f5b6be834ff07b8
Daniel <Danieltsutton@outlook.com>
2022-02-22 19:51:25 +0100

    initial project files

---
+++ lib/Constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const cIndianRed = Color(0xFFC95D63);
+String FAQtext =
+    ("What is PERT.CAL?:\n\nPERT.CAL is a project management tool that assists in estimating the amount of time necessary to finish a project task.\n\n\nPERT.CAL uses three different variables to calculate:\n\nPessimistic: the longest time that an activity might require to complete\n\nOptimistic: the shortest time in which an activity can be completed\n\nMost likely: the completion time of an activity having the highest probability");
+String FAQtitle = ("FAQ");
